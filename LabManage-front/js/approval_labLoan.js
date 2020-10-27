1bb084cdc4ae0c3800099829f4b0b50038a5d4d5
xxl <2427018614@qq.com>
2020-10-27 08:08:01 +0800

    xxl_f_up

--- LabManage-front/js/approval_labLoan.js
+++ LabManage-front/js/approval_labLoan.js
@@ -1,4 +1,5 @@
 $(function(){
+
     // 点击“审批不通过”按钮：蒙版+审批意见 显现
     $(".no_app").click(function(){
         $(".pop").show()
@@ -10,14 +11,19 @@ $(function(){
         $(".pop").hide()
         $("#no_dialog").hide()
     })
-
+    // 设定一个判断值,1为通过，2为不通过
+    var app_status =0
      // ”审批不通过“弹出框:点击“确定”按钮
      $(".no_ok_btn").click(function(){
          if($("#suggest").val() ==""){
-             alert("审批意见为空哦~~~")
+             alert("审批意见为空噢~~~请输入")
+             
          }
-        // $(".pop").hide()
-        // $("#no_dialog").hide()
+         else{
+             $(".pop").hide()
+        $("#no_dialog").hide()
+         }
+        app_status=2
     })
     
 
@@ -27,22 +33,38 @@ $(function(){
     $("#ok_dialog").show()
 })
 
+
   // ”审批通过“弹出框:点击“确定”按钮
   $(".ok_btn").click(function(){
    $(".pop").hide()
    $("#ok_dialog").hide()
+   app_status =1
+   
 })
 
 // 点击”历史“按钮
 $(".hty_btn").click(function(){
-    $(".hty_no").show()
+    $(".table_time").show()
+    $(".table_no").hide()
     $(".app_choice_btn").hide()
+    // 历史中 --->进行判断展示通过或者不通过,1为通过，2为不通过
+    if(app_status==1){
+        $(".hty_ok").show()
+    }
+    if(app_status==2){
+        $(".hty_no").show()
+        $(".no_result textarea").val($("#suggest").val() )
+
+    }
 })
 
 // 点击”审批“按钮
 $(".applab_btn").click(function(){
-    $(".hty_no").hide()
     $(".app_choice_btn").show()
+    $(".table_no").show()
+    $(".hty_ok").hide()
+    $(".hty_no").hide()
+    $(".table_time").hide()
 })
 
 
